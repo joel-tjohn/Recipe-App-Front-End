756aace592bf03a968b6fafa3e3ebd795f5cfd3f
joel-tjohn <joeltjohn10@gmail.com>
2024-04-09 04:33:16 +0530

    Search Recipe Created

---
+++ src/Components/SearchRecipe.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Header from './Header'
+
+
+const SearchRecipe = () => {
+    return (
+        <div>
+
+            
+            <div className="container">
+
+
+                <div className="row col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                            <label htmlFor="" className="form-label">Title</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SearchRecipe
